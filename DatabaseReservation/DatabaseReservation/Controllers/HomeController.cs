f3c3ea047ddfba2983537b898c44a43412144ec6
StyvenZhuo <151433246+StyvenZhuo@users.noreply.github.com>
2024-10-05 22:33:48 +0700

    Database

---
+++ DatabaseReservation/DatabaseReservation/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using DatabaseReservation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DatabaseReservation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy(string username)
+        {
+            Console.WriteLine(username);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
